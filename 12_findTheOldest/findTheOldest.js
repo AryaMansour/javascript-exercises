c50ad1c5ad0d64e29a532c921c34f96ef04e4f1d
AryaMansour <msg_arya@yahoo.ca>
2022-03-29 20:30:18 -0400

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = (old) => {
+    let oldest = 0;
+    let current = 0;
+    let person = []
+    for (i=0; i<old.length; ++i) {
+       current = old[i].yearOfDeath - old[i].yearOfBirth;
+       if (current > oldest) {
+           person = old[i].name
+           oldest = current
+       };
+    };
+    return person
 };
 
 // Do not edit below this line
