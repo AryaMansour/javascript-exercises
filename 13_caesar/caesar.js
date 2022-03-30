e45bd58af52c028b6436f7d8edb4989e1a4d7954
AryaMansour <msg_arya@yahoo.ca>
2022-03-29 21:07:22 -0400

    finished all exercises

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,11 @@
-const caesar = function() {
-
+let decodedString = '';
+const caesarDecode = (string, shift) => {
+    for (i=0; i < string.length; i++) {
+        decodedString += String.fromCharCode((string.charCodeAt(i) + shift))
+    };
+    return decodedString
 };
+
 
 // Do not edit below this line
 module.exports = caesar;
